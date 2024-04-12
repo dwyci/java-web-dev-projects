37d6e6ef9bb54a4bb682dff8c925682cdd335a67
Dallas Wyciskalla <dallas.wyciskalla@gmail.com>
2024-04-11 20:55:01 -0500

    addNew()

--- classes-part-2/studio/src/main/java/org/launchcode/MenuItem.java
+++ classes-part-2/studio/src/main/java/org/launchcode/MenuItem.java
@@ -1,17 +1,25 @@
 package org.launchcode;
 
+import java.util.Scanner;
+
 public class MenuItem {
+    private String name;
     private double price;
     private String description;
     private String category;
     private boolean isNew;
 
-    public MenuItem(double p, String d, String c, boolean iN) {
+    public MenuItem(String n, double p, String d, String c, boolean iN) {
+        this.name = n;
         this.price = p;
         this.description = d;
         this.category = c;
         this.isNew = iN;
     }
+
+    public String getName() { return name;}
+
+    public void setName(String name) { this.name = name;}
 
     public void setPrice(double price) {
         this.price = price;
@@ -27,6 +35,11 @@ public class MenuItem {
 
     public void setNew(boolean aNew) {
         isNew = aNew;
+    }
+
+    public void addNew(){
+        System.out.println("Please enter the name, price, description, category");
+                Scanner input = new Scanner(System.in);
     }
 }
 
---
+++ classes-part-2/studio/src/main/java/org/launchcode/Restaurant.java
@@ -0,0 +1,13 @@
+package org.launchcode;
+
+public class Restaurant {
+
+    public static void main(String[] args) {
+
+
+//        Create several items and add them to a menu.
+//                Print the entire, updated menu to the screen.
+//        Print an individual menu item to the screen.
+//        Delete an item from a menu, then reprint the menu.
+    }
+}
